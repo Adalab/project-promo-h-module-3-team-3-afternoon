2a0ed24cbcd72bcef5b9706334972f5f208251ae
Marian Gonzalez <gonzalezmarian170@gmail.com>
2019-12-16 20:20:09 +0100

    add footer and fix Landing issues

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import './styles/App.scss';
+import Landing from './components/Landing';
+import Footer from './components/Footer';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Landing />
+        <Footer />
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,39 +1,37 @@
 import React from 'react';
 import Logo from '../images/awesome-cards.svg';
 
-class Landing extends React.Component {
-    render() {
-      return (
-        <main class="page__main">
-            <div class="head__awesome-logo">
+const Landing = () => {
+    return (
+        <main className="page__main">
+            <div className="head__awesome-logo">
                 <a href="https://github.com/Adalab/project-promo-h-module-2-team-4-afternoon">
-                    <img class="head__awesome-img" src={Logo} alt="Logo Awesome Cards"></img>
+                    <img className="head__awesome-img" src={Logo} alt="Logo Awesome Cards"></img>
                 </a>
             </div>
-            <div class="main__title">
-                <h1 class="title__text">crea tu tarjeta de visita</h1>
-                <p class="title_paragraph">crea mejores contactos profesionales de forma fácil y cómoda</p>
+            <div className="main__title">
+                <h1 className="title__text">crea tu tarjeta de visita</h1>
+                <p className="title_paragraph">crea mejores contactos profesionales de forma fácil y cómoda</p>
             </div>
 
-            <ul class="main__icons">
-                <li class="main__icons-item">
-                <i class="main__img far fa-object-ungroup"></i>
-                <p class="main_text-item">diseña</p>
+            <ul className="main__icons">
+                <li className="main__icons-item">
+                    <i className="main__img far fa-object-ungroup"></i>
+                    <p className="main_text-item">diseña</p>
                 </li>
-                <li class="main__icons-item">
-                <i class="main__img far fa-keyboard"></i>
-                <p class="main_text-item">rellena</p>
+                <li className="main__icons-item">
+                    <i className="main__img far fa-keyboard"></i>
+                    <p className="main_text-item">rellena</p>
                 </li>
-                <li class="main__icons-item">
-                <i class="main__img fas fa-share-alt"></i>
-                <p class="main_text-item">comparte</p>
+                <li className="main__icons-item">
+                    <i className="main__img fas fa-share-alt"></i>
+                    <p className="main_text-item">comparte</p>
                 </li>
             </ul>
 
-            <a class="main__link" href="./cardpage.html">comenzar</a>
+            <a className="main__link" href="./cardpage.html">comenzar</a>
         </main>
-      );
-    }
+    );
 }
 
 export default Landing;
