bbf584a88ccc5737278cfa09bde5738fa76b7b09
marianalfr <marianalfr@gmail.com>
2019-12-20 13:19:00 +0100

    Add docs

---
+++ docs/precache-manifest.7dfaccbf945ab6b9d4c2f47190fba932.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5043d2b135bdb84dc142a68ea0c01d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "776b9361f61b3701cb6f",
+    "url": "./static/css/main.32d923ee.chunk.css"
+  },
+  {
+    "revision": "caa5738b30e8b5e9ed79",
+    "url": "./static/js/2.2cc52ed8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.2cc52ed8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "776b9361f61b3701cb6f",
+    "url": "./static/js/main.aedd8cd3.chunk.js"
+  },
+  {
+    "revision": "99493b6f5bfcbcbfa9dd",
+    "url": "./static/js/runtime-main.59c71401.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/awesome-cards.0118e311.svg"
+  },
+  {
+    "revision": "28066c7fa3aff8d385d551d1c50014ab",
+    "url": "./static/media/default-avatar.28066c7f.png"
+  }
+]);
